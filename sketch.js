ebdd11e524b7570ffa3d145dee5a87892f4cc76e
LazyJoe666 <Joeao1026@gmail.com>
2024-09-23 23:32:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,38 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function star(x, y, w, c) {
+  let r = w / 2;
+
+  push();
+
+  translate(x, y);
+
+  line(-c, -c, 0, -r);
+  line(0, -r, c, -c);
+
+  line(-c, c, 0, r);
+  line(0, r, c, c);
+
+  line(c, -c, r, 0);
+  line(r, 0, c, c);
+
+  line(-c, -c, -r, 0);
+  line(-r, 0, -c, c);
+  pop();
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(120, 120, 120);
+
+  strokeWeight(2);
+  for (let y = 50; y < height; y += 100) {
+    star(x, 50, 100, 5);
+  }
+  for (let x = 50; x < width; x += 120) {
+    star(x, y, 110, 5);
+ 
+  }
 }
+
+
+
+function draw() {}
